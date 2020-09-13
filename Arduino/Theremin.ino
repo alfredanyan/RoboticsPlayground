f7401802c294525198258638970b43bb7b8aab8b
Alfred Anyan <46558691+alfredanyan@users.noreply.github.com>
2020-09-13 10:46:11 +0000

    Update Theremin.ino

--- Arduino/Theremin.ino
+++ Arduino/Theremin.ino
@@ -1,6 +1,33 @@
+//constant reference tied to the analog in pin
 const int photoRPin = A0;
 
-void setup(){
+//output pin to pass tone to passive buzzer
+const int buzzerOutPin = 11;
 
+//store value for photo resistor to display on screen
+int photoRVal = 0;
+
+// tone value to pass to buzzer
+int toneVal = 0
+
+void setup(){
+  Serial.begin(9600);
+  
+  
+
+
+}
+void loop(){
+  //take reading of analog pin
+  photoRVal = analogRead(photoRPin);
+  //print resistance from photo resistor
+  Serial.print("Photoresistor : ");
+  
+  Serial.println(photoRVal);
+  //getting value of 0 - 1023 from input, but need to keep range for tones between 120 and 1500 using map which takes a range of values and maps the lowest to a specified lower limit and the highest to a specified upper limit.
+  toneVal = map(photoRVal, 0, 1023, 120, 1500);
+  tone(buzzerOutPin, toneVal);
+  //pause long enough to get sensor reading
+  delay(2);
 
 }
